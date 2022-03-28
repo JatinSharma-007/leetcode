ade2e7cc1589d9f1dc541652e9822df160a3fa37
JatinSharma-007 <53529241+JatinSharma-007@users.noreply.github.com>
2022-03-28 22:44:33 +0530

    2 question solved

---
+++ (2)addnumbers.js
@@ -0,0 +1,11 @@
+let l1 = [2, 4, 3];
+let l2 = [5, 6, 4];
+let l3 = 0;
+var addTwoNumbers = function (l1, l2) {
+    for (var i = 0; i < l1.length; i++) {
+        l3 += l1[i] = parseInt(l1[i], 10)
+        console.log(l3)
+    }
+
+};
+addTwoNumbers(l1, l2)
