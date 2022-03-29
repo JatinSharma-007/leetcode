854e2309036833668304611b0c5c201ffb1111f1
JatinSharma-007 <53529241+JatinSharma-007@users.noreply.github.com>
2022-03-29 22:17:52 +0530

    compledted 14th problem

---
+++ (14)commonPrefix.js
@@ -0,0 +1,13 @@
+let strs = ["flower", "flow", "flight"];
+var longestCommonPrefix = function (strs) {
+    if (strs.length === 0) return 0
+    for (let i = 0; i < strs[0].length; i++) {
+        for (let j = 1; j < strs.length; j++) {
+            if (strs[0][i] != strs[j][i]) {
+                return strs[0].slice(0, i)
+            }
+        }
+    }
+    return strs[0]
+};
+console.log(longestCommonPrefix(strs));
