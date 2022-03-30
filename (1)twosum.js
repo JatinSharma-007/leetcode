982fbab49a0ef2361d0d30099e4a2b6c1a1cd21a
JatinSharma-007 <53529241+JatinSharma-007@users.noreply.github.com>
2022-03-30 17:10:30 +0530

    Update (1)twosum.js

--- (1)twosum.js
+++ (1)twosum.js
@@ -1,6 +1,6 @@
 let nums = [2, 7, 11, 15];
 let target = 9;
-                
+                  
 var twoSum = function (nums, target) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
