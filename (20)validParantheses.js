b57d9a268a01f70595d543de34bca48b2d95f976
JatinSharma-007 <53529241+JatinSharma-007@users.noreply.github.com>
2022-03-31 22:46:44 +0530

    completed 5th question

---
+++ (20)validParantheses.js
@@ -0,0 +1,19 @@
+var isValid = function (s) {
+    let queue = [];
+    for (let i = 0; i < s.length; i++) {
+        let first = s[i];
+        let last = queue[queue.length - 1];
+        if (first === ")" && last === "(") {
+            queue.pop();
+        } else if (first === "}" && last === "{") {
+            queue.pop();
+        } else if (first === "]" && last === "[") {
+            queue.pop();
+        } else {
+            queue.push(first);
+        }
+
+    }
+    return !queue.length;
+};
+
