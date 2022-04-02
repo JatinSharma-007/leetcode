555de944ff58fc76e1dfdeb2e7391b75b2994b19
JatinSharma-007 <53529241+JatinSharma-007@users.noreply.github.com>
2022-04-02 21:09:02 +0530

    sorted list

--- (21)mergeSortedList.js
+++ (21)mergeSortedList.js
@@ -2,6 +2,6 @@ let list1 = [1, 2, 4];
 let list2 = [1, 3, 4];
 
 var mergeTwoLists = function (list1, list2) {
-    return list1.concat(list2).sort()
+    return list1.concat(list2).sort();
 };
-console.log(mergeTwoLists(list1, list2))
+console.log(mergeTwoLists(list1, list2));
