49b4f14282bbe6aeafa0899d869b4542fa37c3b9
JatinSharma-007 <53529241+JatinSharma-007@users.noreply.github.com>
2022-04-03 17:13:11 +0530

    sumZero

--- (20)validParantheses.js
+++ (20)validParantheses.js
@@ -12,7 +12,6 @@ var isValid = function (s) {
         } else {
             queue.push(first);
         }
-
     }
     return !queue.length;
 };
---
+++ sumZero.js
@@ -0,0 +1,13 @@
+let arr = [-5, -4, -3, -2, 0, 2, 4, 6, 8];
+let target = 0;
+
+const checkSum = (arr, target) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === target) {
+                return [i, j]
+            }
+        }
+    }
+}
+console.log(checkSum(arr, target));
