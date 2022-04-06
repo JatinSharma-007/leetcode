4b1345da25539616d9f3b50fb9be76347951bffc
JatinSharma-007 <53529241+JatinSharma-007@users.noreply.github.com>
2022-04-06 18:12:04 +0530

    Update (2)addnumbers.js

--- (2)addnumbers.js
+++ (2)addnumbers.js
@@ -3,7 +3,7 @@ let l2 = [5, 6, 4];
 let l3 = 0;
 var addTwoNumbers = function (l1, l2) {
     for (var i = 0; i < l1.length; i++) {
-        l3 += l1[i] = parseInt(l1[i], 10)
+        l3 += l1[i] = parseInt(l1[i], 10) 
          console.log(l3)   
     }
 
