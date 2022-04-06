0ea3437e1e096355410fb64217ea551a883f89cb
JatinSharma-007 <53529241+JatinSharma-007@users.noreply.github.com>
2022-04-06 18:14:10 +0530

    Update (2)addnumbers.js

--- (2)addnumbers.js
+++ (2)addnumbers.js
@@ -1,7 +1,7 @@
 let l1 = [2, 4, 3];
 let l2 = [5, 6, 4];
 let l3 = 0;
-
+ 
 
 var addTwoNumbers = function (l1, l2) {
     for (var i = 0; i < l1.length; i++) { 
@@ -10,4 +10,5 @@ var addTwoNumbers = function (l1, l2) {
     } 
 
 };
+
 addTwoNumbers(l1, l2)
