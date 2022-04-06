f27082644c6de5ea24c1b56cf95d0c88097d954d
JatinSharma-007 <53529241+JatinSharma-007@users.noreply.github.com>
2022-04-06 18:11:52 +0530

    Update (2)addnumbers.js

--- (2)addnumbers.js
+++ (2)addnumbers.js
@@ -4,7 +4,7 @@ let l3 = 0;
 var addTwoNumbers = function (l1, l2) {
     for (var i = 0; i < l1.length; i++) {
         l3 += l1[i] = parseInt(l1[i], 10)
-         console.log(l3)
+         console.log(l3) 
     }
 
 };
