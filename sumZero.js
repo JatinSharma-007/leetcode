beee915466ec1373ea9b69aab928f0653b24635b
JatinSharma-007 <53529241+JatinSharma-007@users.noreply.github.com>
2022-04-07 13:34:58 +0530

    Update sumZero.js

--- sumZero.js
+++ sumZero.js
@@ -1,6 +1,6 @@
 // o(n^2) quadratic time complexity  
 let arr = [-5, -4, -3, -2, 0, 2, 4, 6, 8];
-const checkSum = (arr) => {
+const checkSum = (arr) => { 
     for (let i = 0; i < arr.length; i++) {
         for (let j = i + 1; j < arr.length; j++) {
             if (arr[i] + arr[j] === 0) {
